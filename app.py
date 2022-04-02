ef93f15f87ecbc9723e849c00b80962cc5adba99
Atte Högman <atte.hogman@gmail.com>
2022-04-02 17:34:03 +0300

    initial commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:mypass@localhost:5432/postgres"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 @app.route("/")

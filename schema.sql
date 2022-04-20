9dbf66277dc52705ac3fdda89e8925e0ab1b56cf
Atte Högman <atte.hogman@gmail.com>
2022-04-21 02:25:38 +0300

    modified tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
 CREATE TABLE apartments (id SERIAL PRIMARY KEY, area INT, rooms INT, building TEXT, location TEXT, rent INT, condition TEXT, descr TEXT);
-CREATE TABLE apartments (id SERIAL PRIMARY KEY, area INT, rooms INT, building TEXT, location TEXT, rent INT, condition TEXT, descr TEXT);
 CREATE TABLE applied (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, apartment_id INTEGER REFERENCES apartments);
 CREATE TABLE faved (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, apartment_id INTEGER REFERENCES apartments);
+CREATE TABLE admins (id SERIAL PRIMARY KEY, username TEXT, password TEXT);

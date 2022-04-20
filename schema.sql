fafe561079b759327c1a779bc17c0a68b92c550f
Atte Högman <atte.hogman@gmail.com>
2022-04-21 02:19:30 +0300

    modified schema

--- schema.sql
+++ schema.sql
@@ -3,4 +3,3 @@ CREATE TABLE apartments (id SERIAL PRIMARY KEY, area INT, rooms INT, building TE
 CREATE TABLE apartments (id SERIAL PRIMARY KEY, area INT, rooms INT, building TEXT, location TEXT, rent INT, condition TEXT, descr TEXT);
 CREATE TABLE applied (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, apartment_id INTEGER REFERENCES apartments);
 CREATE TABLE faved (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, apartment_id INTEGER REFERENCES apartments);
-

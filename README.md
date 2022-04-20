7f26be81f16c5d3422dfcc9f453c940de0dacb20
Atte Högman <atte.hogman@gmail.com>
2022-04-21 01:57:36 +0300

    Added apply/favorite -features, order-by, admin rights and fixed login/registration pages

--- README.md
+++ README.md
@@ -18,9 +18,17 @@ kuvauksessa esiintyvien sanojen/merkkijonojen perusteella
 
 Tämänhetkiset ominaisuudet:
 
-- Käyttäjä voi kirjautua sisään ja ulos sekä luoda uuden tunnuksen (HUOM! käyttäjätunnuksen ja salasanan oltava vähintään 6 merkkiä pitkät!)
+- Käyttäjä voi kirjautua sisään ja ulos sekä luoda uuden tunnuksen (Käyttäjätunnuksen ja salasanan oltava vähintään 6 merkkiä pitkät!)
 - Käyttäjä näkee etusivulla tarjolla olevat asunnot lisäysjärjestyksessä
-- Käyttäjä (tässä vaiheessa ei vielä jaoteltuja ylläpitäjiä ja normikäyttäjiä) voi lisätä tai poistaa vuokrattavia sekä määrittää asunnoista näytettävät tiedot
-- Käyttäjä voi etsiä asuntoja pinta-alan, huonemäärän, rakennustyypin, sijainnin (tässä vaiheessa vain kaupungin), vuokran suuruuden sekä kunnon perusteella
+- Käyttäjä voi lisätä asunnon suosikkeihin ja/tai lähettää siihen hakemuksen
+- Käyttäjä voi poistaa asunnon suosikeista ja/tai haetuista
+- Käyttäjä voi lisätä vuokrattavia asuntoja sivustolle (poikkeaa alkuperäisestä suunnitelmasta siten, että ylläpitäjän lisäksi tämä on mahdollista myös peruskäyttäjällä)
+- Käyttäjä voi etsiä asuntoja pinta-alan, huonemäärän, rakennustyypin, sijainnin (kaupungin), vuokran suuruuden sekä kunnon perusteella
+- Käyttäjä voi järjestää asunnot vuokran suuruuden tai koon perusteella
+- Ylläpitäjä voi poistaa vuokrattavia asuntoja
+
+Kohta "Ylläpitäjä voi tarvittaessa lisätä, poistaa..." osoittautui projektin miellekkyttä
+ajatellen turhaksi, joten se jää toteuttamatta. Lisäksi sijaintien tarkkuus jää kaupunkitasolle.
+Muilta osin projekti on saavuttanut kaikki alkuperäiset perustoiminnallisuuden tavoitteet.
 
 Sovellusta voi testata Herokussa osoitteessa https://vuokra-asuntosovellus.herokuapp.com
--- app.py
+++ app.py
@@ -15,26 +15,51 @@ def index():
     apartments = result.fetchall()
     return render_template("index.html", count=len(apartments), apartments=apartments)
 
+@app.route("/order", methods=["POST", "GET"])
+def order_cheap():
+    try:
+        apartment_order = request.form["order"]
+        if apartment_order == "cheap":
+            result = db.session.execute("SELECT * FROM apartments ORDER BY rent")
+            results = result.fetchall()
+        elif apartment_order == "expensive":
+            result = db.session.execute("SELECT * FROM apartments ORDER BY rent DESC")
+            results = result.fetchall()
+        elif apartment_order == "small":
+            result = db.session.execute("SELECT * FROM apartments ORDER BY area")
+            results = result.fetchall()
+        elif apartment_order == "big":
+            result = db.session.execute("SELECT * FROM apartments ORDER BY area DESC")
+            results = result.fetchall()
+
+        return render_template("order.html", results=results)
+    except:
+        return redirect("/")
+
 @app.route("/login",methods=["POST", "GET"])
 def login():
     username = request.form["username"]
     password = request.form["password"]
-    # Tarkistetaan onko käyttäjä olemassa
-    sql = "SELECT id, password FROM users WHERE username=:username"
+    
+    if "admin" in username:
+        sql = "SELECT id, password FROM admins WHERE username=:username"
+    else:
+        sql = "SELECT id, password FROM users WHERE username=:username"
+
     result = db.session.execute(sql, {"username": username})
     user = result.fetchone()
 
     if not user:
-        # Jos käyttäjä ei olemassa, sivu päivittyy
-        return redirect("/")
+        no_user = True
+        return render_template("index.html", no_user=no_user)
     else:
         hash_value = user.password
         if check_password_hash(hash_value, password):
             session["username"] = username     # Voi kirjautua
             return redirect("/")
         else:
-            # Jos salasana väärin, sivu päivittyy
-            return redirect("/")
+            no_user = True
+            return render_template("index.html", no_user=no_user)
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
@@ -42,16 +67,50 @@ def register():
 
 @app.route("/create",methods=["POST", "GET"])
 def create():
+    name_taken = False
+    too_long_user = False
+    too_short_user = False
+    too_long_pass = False
+    too_short_pass = False
+    pws_not_equal = False
     username = request.form["username"]
     password = request.form["password"]
-    # Jos valitut tunnukset yli 5 merkkiä, luodaan käyttäjä
-    if len(username)>5 and len(password)>5:
+    password_again = request.form["password_again"]
+
+    if password != password_again:
+        pws_not_equal = True
+
+    sqlusers = f"SELECT username FROM users WHERE username='{username}'"
+    result = db.session.execute(sqlusers)
+    user = result.fetchone()
+
+    if user:
+        name_taken = True
+
+    if 15<len(username) and not name_taken and not pws_not_equal:
+        too_long_user = True
+    elif 6>len(username) and not name_taken and not pws_not_equal:
+        too_short_user = True
+
+    if 15<len(password) and not name_taken and not pws_not_equal:
+        too_long_pass = True
+    elif 6>len(password) and not name_taken and not pws_not_equal:
+        too_short_pass = True
+
+    if 15>=len(username)>=6 and 15>=len(password)>=6 and not name_taken and not pws_not_equal:
         hash_value = generate_password_hash(password)
-        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        if "admin" not in username:
+            sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        else:
+            sql = "INSERT INTO admins (username, password) VALUES (:username, :password)"
         db.session.execute(sql, {"username": username, "password": hash_value})
         db.session.commit()
+        session["username"] = username
         return redirect("/")
-    return redirect(url_for("register"))
+
+    return render_template("register.html", name_taken=name_taken, too_long_user=too_long_user,
+                           too_short_user=too_short_user, too_long_pass=too_long_pass,
+                           too_short_pass=too_short_pass, pws_not_equal=pws_not_equal)
 
 @app.route("/palaa")
 def palaa():
@@ -66,7 +125,8 @@ def result():
     roomcount = request.args.getlist("roomcount")
     buildingtype = request.args.getlist("buildingtype")
     condition = request.args.getlist("condition")
-    if query == "" and min_area == "" and max_area == "" and len(roomcount)==0 and len(buildingtype)==0 and len(condition)==0:
+    if query == "" and min_area == "" and max_area == "" and \
+            len(roomcount)==0 and len(buildingtype)==0 and len(condition)==0:
         sql = "SELECT * FROM apartments"
     else:
         sql = "SELECT * FROM apartments WHERE "
@@ -157,7 +217,7 @@ def result():
 
     result = db.session.execute(sql)
     results = result.fetchall()
-    return render_template("result.html", results=results)
+    return render_template("result.html", resultslen=len(results), results=results)
 
 @app.route("/new")
 def new():
@@ -172,10 +232,173 @@ def add():
     rent = request.form["rent"]
     condition = request.form["condition"]
     descr = request.form["descr"]
-    sql = f"INSERT INTO apartments (area, rooms, building, location, rent, condition, descr) VALUES ({area}, {roomcount}, '{buildingtype}', '{location}', {rent}, '{condition}', '{descr}');"
+    sql = f"INSERT INTO apartments (area, rooms, building, location, rent, condition, descr)" \
+          f" VALUES ({area}, {roomcount}, '{buildingtype}', '{location}', {rent}, '{condition}', '{descr}');"
     db.session.execute(sql)
     db.session.commit()
     return redirect("/")
+
+@app.route("/add_applied_or_fave", methods=["GET", "POST"])
+def add_applied_or_fave():
+
+    username = session["username"]
+    user_id_search = f"SELECT id FROM users WHERE username='{username}'"
+    user_result = db.session.execute(user_id_search)
+    user_id = user_result.fetchone()[0]
+
+    apply = request.form.getlist("apply")
+    fave = request.form.getlist("fave")
+
+    for i in range(0, len(apply)):
+        sql = f"INSERT INTO applied (user_id, apartment_id) VALUES ({user_id}, {apply[i]})"
+        db.session.execute(sql)
+        db.session.commit()
+
+    for i in range(0, len(fave)):
+        sql = f"INSERT INTO faved (user_id, apartment_id) VALUES ({user_id}, {fave[i]})"
+        db.session.execute(sql)
+        db.session.commit()
+
+
+    return redirect("/")
+
+
+@app.route("/appandfav", methods=["GET", "POST"])
+def appandfav():
+    username = session["username"]
+    user_id_search = f"SELECT id FROM users WHERE username='{username}'"
+    user_result = db.session.execute(user_id_search)
+    user_id = user_result.fetchone()[0]
+
+    applied_sql = f"SELECT apartment_id FROM applied WHERE user_id={user_id}"
+    applied_result = db.session.execute(applied_sql)
+    applied_results = applied_result.fetchall()
+
+    faved_sql = f"SELECT apartment_id FROM faved WHERE user_id={user_id}"
+    faved_result = db.session.execute(faved_sql)
+    faved_results = faved_result.fetchall()
+
+    a_sql_const = True
+    f_sql_const = True
+    a_sql = ""
+    f_sql = ""
+    a_results = []
+    f_results = []
+
+    if len(applied_results)>=1:
+        for i in applied_results:
+            b = str(i).strip("(),")
+            if a_sql_const:
+                a_sql = f"SELECT * FROM apartments WHERE id={int(b)}"
+                a_sql_const = False
+            else:
+                a_sql += f" OR id={int(b)}"
+            a_result = db.session.execute(a_sql)
+            a_results = a_result.fetchall()
+
+
+    if len(faved_results)>=1:
+        for i in faved_results:
+            b = str(i).strip("(),")
+            if f_sql_const:
+                f_sql = f"SELECT * FROM apartments WHERE id={int(b)}"
+                f_sql_const = False
+            else:
+                f_sql += f" OR id={int(b)}"
+
+            f_result = db.session.execute(f_sql)
+            f_results = f_result.fetchall()
+
+    return render_template("appandfav.html", a_resultslen=len(a_results),
+                           f_resultslen=len(f_results), a_results=a_results, f_results=f_results)
+
+
+@app.route("/delete", methods=["GET", "POST"])
+def delete():
+    result = db.session.execute("SELECT * FROM apartments ORDER BY id DESC")
+    apartments = result.fetchall()
+    return render_template("delete.html", apartments=apartments)
+
+@app.route("/delete_apartment", methods=["GET", "POST"])
+def delete_apartment():
+    delete = request.form.getlist("apartment")
+    first = True
+    sql_applied = ""
+    sql_faved = ""
+    sql_apartments = ""
+    for i in delete:
+        if first:
+            sql_applied = f"DELETE FROM applied WHERE apartment_id={i}"
+            sql_faved = f"DELETE FROM faved WHERE apartment_id={i}"
+            sql_apartments = f"DELETE FROM apartments WHERE id={i}"
+            first = False
+            continue
+        sql_applied += f" OR apartment_id={i}"
+        sql_faved += f" OR apartment_id={i}"
+        sql_apartments += f" OR id={i}"
+    db.session.execute(sql_applied)
+    db.session.execute(sql_faved)
+    db.session.execute(sql_apartments)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/manipulate_applied_and_faved", methods=["GET", "POST"])
+def manipulate_applied_and_faved():
+    username = session["username"]
+    user_id_search = f"SELECT id FROM users WHERE username='{username}'"
+    user_result = db.session.execute(user_id_search)
+    user_id = user_result.fetchone()[0]
+
+    delete_applied = request.form.getlist("delete_applied")
+    delete_faved = request.form.getlist("delete_faved")
+    fave = request.form.getlist("fave")
+    apply = request.form.getlist("apply")
+    first = True
+
+    sql = ""
+    for i in delete_applied:
+        if first:
+            sql = f"DELETE FROM applied WHERE user_id='{user_id}' and apartment_id={i}"
+            first = False
+            continue
+        sql += f" OR apartment_id={i}"
+
+    if len(delete_applied)>0:
+        db.session.execute(sql)
+        db.session.commit()
+
+    sql = ""
+    for i in fave:
+        sql = f"INSERT INTO faved (user_id, apartment_id) VALUES ({user_id}, {i})"
+
+    if len(fave) > 0:
+        db.session.execute(sql)
+        db.session.commit()
+
+
+    first = True
+    sql = ""
+    for i in delete_faved:
+        if first:
+            sql = f"DELETE FROM faved WHERE user_id='{user_id}' and apartment_id={i}"
+            first = False
+            continue
+        sql += f" OR apartment_id={i}"
+
+    if len(delete_faved)>0:
+        db.session.execute(sql)
+        db.session.commit()
+
+    sql = ""
+    for i in apply:
+        sql = f"INSERT INTO applied (user_id, apartment_id) VALUES ({user_id}, {i})"
+
+    if len(apply) > 0:
+        db.session.execute(sql)
+        db.session.commit()
+
+    return redirect("/appandfav")
+
 
 @app.route("/logout")
 def logout():
